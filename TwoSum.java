44c238b4f60a16ab1e6c2ed49def284546b233a3
dahuin-1 <dawhite01@naver.com>
2021-07-05 18:20:34 +0900

    first commit

---
+++ TwoSum.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        TwoSum t = new TwoSum();
+        int[] a = new int[]{2, 7, 11, 15};
+        System.out.println(t.twoSum(a,9));
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] answer = {};
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int newTarget = target - nums[i];
+            if (map.containsKey(newTarget) && i != map.get(newTarget)) {
+                answer = new int[]{i, map.get(newTarget)};
+                return answer;
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
