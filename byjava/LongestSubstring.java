9c7b4374afff1403b683c905ab2099efceb11b1e
dahuin-1 <dawhite01@naver.com>
2021-07-13 22:48:20 +0900

    add longestsubstring

--- byjava/LongestSubstring.java
+++ byjava/LongestSubstring.java
@@ -5,36 +5,27 @@ public class LongestSubstring {
         LongestSubstring ls = new LongestSubstring();
         ls.lengthOfLongestSubstring("pwwkew");
     }
-
     public int lengthOfLongestSubstring(String s) {
-        //s = abc abc bb
         String[] list = new String[s.length()];
-        StringBuilder str = new StringBuilder();
-
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             for (int k = i; k < s.length(); k++) {
-                if (!str.toString().contains(String.valueOf(s.charAt(k)))) {
-                    str.append(s.charAt(k));
+                if (!stringBuilder.toString().contains(String.valueOf(s.charAt(k)))) {
+                    stringBuilder.append(s.charAt(k));
                 } else {
                     break;
                 }
             }
-
-        list[i] = String.valueOf(str);
-        str = new StringBuilder();
-    }
-
-        int max = 0;
-        for (int i = 0; i < list.length; i++) {
-           // System.out.println(list[i].length());
-           if(max < list[i].length()) {
-               max = list[i].length();
-           }
+            list[i] = String.valueOf(stringBuilder);
+            stringBuilder = new StringBuilder();
         }
-       // System.out.println(max);
-
+        int max = 0;
+        for (String value : list) {
+            if (max < value.length()) {
+                max = value.length();
+            }
+        }
         return max;
-
     }
 
 }
