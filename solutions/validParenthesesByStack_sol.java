ba6262eed7c3e85037d8ef5501d67b2379c3bbc8
dahuin-1 <dawhite01@naver.com>
2021-07-18 15:19:28 +0900

    update Valid Parentheses by stack

--- solutions/validParenthesesByStack_sol.java
+++ solutions/validParenthesesByStack_sol.java
@@ -3,13 +3,20 @@ package solutions;
 import java.util.Stack;
 
 public class validParenthesesByStack_sol {
-
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<Character>();
-        for(char c : s.toCharArray()){
-
+        for(int i = 0; i < s.length(); i++){
+        if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
+            stack.push(s.charAt(i));
+        else if(s.charAt(i) == ')' && !stack.empty() && stack.peek() == '(')
+            stack.pop();
+        else if(s.charAt(i) == ']' && !stack.empty() && stack.peek() == '[')
+            stack.pop();
+        else if(s.charAt(i) == '}' && !stack.empty() && stack.peek() == '{')
+            stack.pop();
+        else
+            return false;
         }
         return stack.isEmpty();
     }
-
 }
