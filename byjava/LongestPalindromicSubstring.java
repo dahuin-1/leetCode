73a8cab479ce1dc75071c9a258dae5ac526a63db
dahuin-1 <dawhite01@naver.com>
2021-07-18 16:37:04 +0900

    add Longest Palindromic Substring

--- byjava/LongestPalindromicSubstring.java
+++ byjava/LongestPalindromicSubstring.java
@@ -1,33 +1,35 @@
 package byjava;
 
-public class LongestSubstring {
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestPalindromicSubstring {
     public static void main(String[] args) {
-        LongestSubstring ls = new LongestSubstring();
-        System.out.println(ls.lengthOfLongestSubstring("pwwkew"));
+        LongestPalindromicSubstring lps = new LongestPalindromicSubstring();
+        lps.longestPalindrome("babcabtad");
     }
 
-    public int lengthOfLongestSubstring(String s) {
+    public String longestPalindrome(String s) {
         String[] list = new String[s.length()];
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            for (int k = i; k < s.length(); k++) {
-                if (!stringBuilder.toString().contains(String.valueOf(s.charAt(k)))) {
+            for (int k = i+1; k < s.length(); k++) {
+                stringBuilder.append(s.charAt(k-1));
+                if (s.charAt(i) == s.charAt(k)) {
                     stringBuilder.append(s.charAt(k));
-                } else {
+                   break;
+                }/* else {
                     break;
-                }
+                }*/
             }
             list[i] = String.valueOf(stringBuilder);
             stringBuilder = new StringBuilder();
         }
-        int max = 0;
         for (String value : list) {
             System.out.println(value);
-            if (max < value.length()) {
-                max = value.length();
-            }
         }
-        return max;
+
+        return "";
     }
 
 }
