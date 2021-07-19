193386b1f2e394dd3734d99338dc5d1b9b77caf2
dahuin-1 <dawhite01@naver.com>
2021-07-19 21:57:53 +0900

    add longestPalindrome

--- byjava/LongestPalindromicSubstring.java
+++ byjava/LongestPalindromicSubstring.java
@@ -1,17 +1,43 @@
 package byjava;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class LongestPalindromicSubstring {
+
     public static void main(String[] args) {
         LongestPalindromicSubstring lps = new LongestPalindromicSubstring();
-        lps.longestPalindrome("ccc");
+       System.out.println(lps.longestPalindrome("ccc"));
     }
 
     public String longestPalindrome(String s) {
+        String result = "", temp = "";
 
-        return "";
+        if(s.length() < 2){
+            return s;
+        }
+
+        for (int i = 0; i < s.length(); i++){
+            //ex) aba
+            temp = getLongestPalindrome(s, i, i);
+            if (result.length() < temp.length()) {
+                result = temp;
+            }
+            //ex) abba
+            temp = getLongestPalindrome(s, i, i+1);
+            if (result.length() < temp.length()) {
+                result = temp;
+            }
+        }
+        return result;
+    }
+
+    private String getLongestPalindrome(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l = l - 1;
+            r = r + 1;
+            //if(l < 0 || r > s.length()) {
+             //   return "";
+           // }
+        }
+        return s.substring(l+1,r);
     }
 
 }
