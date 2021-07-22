39d1c2f5fa86ba74e73b251b934baf5d5cfad9dd
dahuin-1 <dawhite01@naver.com>
2021-07-23 00:01:51 +0900

    ValidateBinarySearchTree_98

--- byjava/ValidateBinarySearchTree_98.java
+++ byjava/ValidateBinarySearchTree_98.java
@@ -1,32 +1,31 @@
 package byjava;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
 public class ValidateBinarySearchTree_98 {
     public static void main(String[] args) {
-
     }
 
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
+        if (root == null) {
+            return true;
+        }
         Stack<TreeNode> stack = new Stack<>();
         TreeNode currentNode = root;
-
-        while(currentNode != null || !stack.empty()){
+        TreeNode pre = null;
+        while(currentNode != null || !stack.empty()) {
             while(currentNode != null){
                 stack.add(currentNode);
-               // TreeNode left = currentNode.left;
-                if(currentNode.val < currentNode.left.val) {
-                    return false;
-                }
                 currentNode = currentNode.left;
             }
             currentNode = stack.pop();
+            if(pre != null && currentNode.val <= pre.val) {
+                return false;
+            }
+            pre = currentNode;
             currentNode = currentNode.right;
         }
-        return stack.isEmpty();
+        return true;
     }
 
 
