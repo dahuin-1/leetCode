be880275c7f6ffbd680048aae5df7cc3b1ebfe92
dahuin-1 <dawhite01@naver.com>
2021-08-03 12:19:02 +0900

    add twosum

--- Top100andTopInterview/TwoSum.java
+++ Top100andTopInterview/TwoSum.java
@@ -5,24 +5,19 @@ import java.util.Map;
 
 public class TwoSum {
 
-    public static void main(String[] args) {
-        TwoSum t = new TwoSum();
-        int[] a = new int[]{2, 7, 11, 15};
-        System.out.println(t.twoSum(a,9));
-    }
-
     public int[] twoSum(int[] nums, int target) {
-        int[] answer = new int[2];
+        int[] result = new int[2];
         Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            int tempTarget = target - nums[i];
-            if(map.containsKey(tempTarget)) {
-                answer[0] = map.get(tempTarget);
-                answer[1] = i;
-                return answer;
+        //k,v get(k) -> v
+        //value, index
+        for (int i = 0; i < nums.length; i++) {
+            int tempTarget = target - nums[i]; //key(=num)
+            if(map.containsKey(tempTarget)){
+                result[0] = i;
+                result[1] = map.get(tempTarget); //index
             }
-            map.put(nums[i], i);
+            map.put(i,nums[i]);
         }
-       return answer;
+        return result;
     }
 }
