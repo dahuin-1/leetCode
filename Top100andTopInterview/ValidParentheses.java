2bb9a4b55a47a4051b8ba4b04888e6a09c17540b
dahuin-1 <dawhite01@naver.com>
2021-08-06 01:49:31 +0900

    add ValidParentheses

--- Top100andTopInterview/ValidParentheses.java
+++ Top100andTopInterview/ValidParentheses.java
@@ -17,6 +17,7 @@ public class ValidParentheses {
                 return false;
         }
         return stack.isEmpty();
+
     }
 
 
--- solutions/validParenthesesByStack_sol.java
+++ solutions/validParenthesesByStack_sol.java
@@ -5,18 +5,20 @@ import java.util.Stack;
 public class validParenthesesByStack_sol {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<Character>();
-        for(int i = 0; i < s.length(); i++){
-        if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
-            stack.push(s.charAt(i));
-        else if(s.charAt(i) == ')' && !stack.empty() && stack.peek() == '(')
-            stack.pop();
-        else if(s.charAt(i) == ']' && !stack.empty() && stack.peek() == '[')
-            stack.pop();
-        else if(s.charAt(i) == '}' && !stack.empty() && stack.peek() == '{')
-            stack.pop();
-        else
-            return false;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
+                stack.push(s.charAt(i));
+            else if (s.charAt(i) == ')' && !stack.empty() && stack.peek() == '(')
+                stack.pop();
+            else if (s.charAt(i) == ']' && !stack.empty() && stack.peek() == '[')
+                stack.pop();
+            else if (s.charAt(i) == '}' && !stack.empty() && stack.peek() == '{')
+                stack.pop();
+            else
+                return false;
         }
         return stack.isEmpty();
     }
+    //[{}]
+    //
 }
