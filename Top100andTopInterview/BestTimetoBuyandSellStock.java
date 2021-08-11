4d3194cd14a4487c34beb783226831565eb72126
dahuin-1 <dawhite01@naver.com>
2021-08-11 17:58:34 +0900

    add BestTimetoBuyandSellStock

--- Top100andTopInterview/BestTimetoBuyandSellStock.java
+++ Top100andTopInterview/BestTimetoBuyandSellStock.java
@@ -6,23 +6,18 @@ import java.util.Map;
 public class BestTimetoBuyandSellStock {
     public static void main(String[] args) {
         BestTimetoBuyandSellStock best = new BestTimetoBuyandSellStock();
-        int[] prices = new int[]{7,1,5,3,6,4};
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
         System.out.println(best.maxProfit(prices));
     }
 
     public int maxProfit(int[] prices) {
         int max = 0;
-        int[] dp = new int[prices.length + 1];
-       // Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0 ; i < prices.length -1; i++) {
-            int count = i;
-            while (count < prices.length) {
-                //if (prices[count] > prices[i]) {
-                   // int temp = prices[count] - prices[i];
-                    max = Math.max(prices[count] - prices[i], max);
-                //}
-                count++;
-            }
+        int min = Integer.MAX_VALUE;
+        for (int price : prices) {
+            if (price < min)
+                min = price;
+            else if (price > min)
+                max = Math.max(price - min, max);
         }
         return max;
     }
