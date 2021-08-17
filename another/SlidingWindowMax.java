eeba750b77de3d224e2b9867a993271bb29a24d6
dahuin-1 <dawhite01@naver.com>
2021-08-18 00:12:58 +0900

    add maxSlidingWindow

--- another/SlidingWindowMax.java
+++ another/SlidingWindowMax.java
@@ -1,5 +1,7 @@
 package another;
 
+import java.util.Map;
+
 public class SlidingWindowMax {
     public static void main(String[] args) {
         int[] a = {2,1,3,4,6,3,8,9,10};
@@ -7,24 +9,24 @@ public class SlidingWindowMax {
         slidingWindowMax.maxSlidingWindow(a,4);
     }
     public int[] maxSlidingWindow(int[] nums, int k) {
-        final int[] max_left = new int[nums.length];
-        final int[] max_right = new int[nums.length];
+        final int[] left = new int[nums.length];
+        final int[] right = new int[nums.length];
 
-        max_left[0] = nums[0];
-        max_right[nums.length - 1] = nums[nums.length - 1];
+        left[0] = nums[0];
+        right[nums.length - 1] = nums[nums.length - 1];
 
-        for (int i = 1; i < nums.length; i++) {
-            max_left[i] = (i % k == 0) ? nums[i] : Math.max(max_left[i - 1], nums[i]);
+        for(int i = 1; i < nums.length; i++) {
+            left[i] = (i % k) == 0 ? nums[i] : Math.max(nums[i], left[i-1]);
 
             final int j = nums.length - i - 1;
-            max_right[j] = (j % k == 0) ? nums[j] : Math.max(max_right[j + 1], nums[j]);
+            right[j] = (j % k) == 0 ? nums[i] : Math.max(nums[j], right[j+1]);
         }
 
-        final int[] sliding_max = new int[nums.length - k + 1];
+        int[] slidingWindow = new int[nums.length - k + 1];
         for (int i = 0, j = 0; i + k <= nums.length; i++) {
-            sliding_max[j++] = Math.max(max_right[i], max_left[i + k - 1]);
+            slidingWindow[j++] = Math.max(right[i], left[i + k - 1]);
         }
 
-        return sliding_max;
+        return slidingWindow;
     }
 }
