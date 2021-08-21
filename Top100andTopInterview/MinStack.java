305c57394694d90b7a1e72b2f1e82abee3bd8ef5
dahuin-1 <dawhite01@naver.com>
2021-08-21 17:48:00 +0900

    minStack

--- Top100andTopInterview/MinStack.java
+++ Top100andTopInterview/MinStack.java
@@ -1,16 +1,22 @@
 package Top100andTopInterview;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MinStack {
 
-    ArrayList<Integer> stack;
+    List<List<Integer>> stack;
 
     public static void main(String[] args) {
         MinStack obj = new MinStack();
         obj.push(5);
         obj.push(6);
         obj.push(7);
+        System.out.println(obj);
+        System.out.println(obj.getMin());
+        obj.pop();
+        System.out.println(obj);
+
     }
     /** initialize your data structure here. */
     public MinStack() {
@@ -19,23 +25,24 @@ public class MinStack {
 
     public void push(int val) {
         //int stackSize = stack.size();
-        if(stack.isEmpty()) {
-            stack.add(val);
-        }else{
+        List sublist = new ArrayList();
+        //if(stack.isEmpty()) {
+            sublist.add(val);
+            stack.add(sublist);
+      //  }else{
            // stack = new ArrayList<>(stack.size()+1);
-            int stackSize = stack.size();
+           /* int stackSize = stack.size();
             System.out.println("stack size - 1 = " + stackSize);
-            stack.size() = stackSize + 1;
-            stack.add(0,val);
-            stack.
-        }
-        System.out.println(stack);
+            stack.add(stackSize + 1, stack.get(stackSize));*/
+      //  }
+       // System.out.println(stack);
     }
 
 
-
     public void pop() {
-
+      // for(int i = stack.size()-1; i == 0; i--){
+           stack.remove(stack.get(stack.size()-1));
+     //  }
     }
 
     public int top() {
@@ -43,6 +50,14 @@ return 1;
     }
 
     public int getMin() {
-return 0;
+        int min = Integer.MAX_VALUE;
+        for (List<Integer> list : stack) {
+            if (min > list.get(0)) {
+                min = list.get(0);
+            }
+            //System.out.println(min);
+            //return min;
+        }
+        return min;
     }
 }
