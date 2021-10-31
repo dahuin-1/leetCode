8909b4cf9531ab75ff239dc2000c5803dfa695e4
dahuin-1 <dawhite01@naver.com>
2021-11-01 01:02:56 +0900

    add LongestSubstringWithoutRepeatingChar

--- Top100andTopInterview/medium/LongestSubstringWithoutRepeatingChar.java
+++ Top100andTopInterview/medium/LongestSubstringWithoutRepeatingChar.java
@@ -8,18 +8,18 @@ import java.util.Set;
 public class LongestSubstringWithoutRepeatingChar {
 
     public int lengthOfLongestSubstring(String s) {
-       int result = 0;
-       char[] arr = s.toCharArray();
-       Set<Character> set = new HashSet<>();
-       for (int p1 = 1, p2 = 0; p1 <= s.length(); p1++){
-           char current = arr[p1-1];
-           if(set.contains(current)){
-               set.remove(arr[p2]);
-               p2++;
-           }
-           set.add(current);
-           result = Math.max(result, p1-p2);
-       }
+        char[] arr = s.toCharArray();
+        int result = 0;
+        Set<Character> set = new HashSet<>();
+        for(int p1=0, p2=1; p2 <= s.length(); p2++) {
+            char current = arr[p2-1];
+            if(set.contains(current)){
+                set.remove(arr[p1]);
+                p1++;
+            }
+            set.add(current);
+            result = Math.max(result,p1-p2);
+        }
         return result;
     }
 
