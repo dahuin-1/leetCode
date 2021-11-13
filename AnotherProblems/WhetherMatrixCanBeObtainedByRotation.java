3a5b3a27c76efee8b849393ff1efdfdb7962031e
dahuin-1 <dawhite01@naver.com>
2021-11-14 01:25:23 +0900

    add Determine Whether Matrix Can Be Obtained By Rotation

--- AnotherProblems/WhetherMatrixCanBeObtainedByRotation.java
+++ AnotherProblems/WhetherMatrixCanBeObtainedByRotation.java
@@ -10,12 +10,32 @@ public class WhetherMatrixCanBeObtainedByRotation {
         w.findRotation(mat, target);
     }
     public boolean findRotation(int[][] mat, int[][] target) {
-
+        int[][] result = mat;
+        for(int i = 0; i < mat.length * 2; i++) {
+            swapRows(result);
+            transpose(result);
+            if(Arrays.deepEquals(result, target)) {
+                return true;
+            }
+        }
+        return false;
     }
     private void transpose(int[][] result) {
-
+        for(int i = 0 ; i < result.length; i++) {
+            for (int j = i + 1; j < result.length; j++) {
+                int temp = result[i][j];
+                result[i][j] = result[j][i];
+                result[j][i] = temp;
+            }
+        }
     }
     private void swapRows(int[][] result) {
-
+        int high = result.length - 1;
+        int low = 0;
+        while (low < high) {
+            int[] temp = result[low];
+            result[low++] = result[high];
+            result[high--] = temp;
+        }
     }
 }
