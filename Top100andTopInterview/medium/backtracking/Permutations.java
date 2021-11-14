c124b39bd5ecb44bd6a5312c68ba184ba844b9d5
dahuin-1 <dawhite01@naver.com>
2021-11-14 22:33:04 +0900

    study to backtracking

--- Top100andTopInterview/medium/backtracking/Permutations.java
+++ Top100andTopInterview/medium/backtracking/Permutations.java
@@ -1,5 +1,5 @@
-package Top100andTopInterview.medium;
-
+package Top100andTopInterview.medium.backtracking;
+//https://leetcode.com/problems/permutations/discuss/18239/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partioning)
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,18 +10,15 @@ public class Permutations {
         if (nums.length == 0) {
             return permutations;
         }
-
         collectPermutations(nums, 0, new ArrayList<>(), permutations);
         return permutations;
     }
 
-    private void collectPermutations(int[] nums, int start, List<Integer> permutation,List<List<Integer>>  permutations) {
-
+    private void collectPermutations(int[] nums, int start, List<Integer> permutation, List<List<Integer>> permutations) {
         if (permutation.size() == nums.length) {
             permutations.add(permutation);
             return;
         }
-
         for (int i = 0; i <= permutation.size(); i++) {
             List<Integer> newPermutation = new ArrayList<>(permutation);
             newPermutation.add(i, nums[start]);
