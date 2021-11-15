4dffdabd7337f3d4d76ac147108864565c7e02e7
dahuin-1 <dawhite01@naver.com>
2021-11-16 01:46:49 +0900

    add Subsets

--- Top100andTopInterview/medium/backtracking/Subsets.java
+++ Top100andTopInterview/medium/backtracking/Subsets.java
@@ -14,5 +14,12 @@ public class Subsets {
     }
 
     private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
+        list.add(new ArrayList<>(tempList));
+        for (int i = start; i < nums.length; i++) {
+           // if(tempList.contains(nums[i]) || nums[i] == nums[i+1]) continue;
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
     }
 }
